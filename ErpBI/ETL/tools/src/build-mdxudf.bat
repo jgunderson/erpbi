b4436bed4f2331af50e0a37e67076a00be3f8998
jgunderson <jgunderson@erpbisolutions.com>
2014-09-18 08:38:58 -0400

    first attempt to convert to pentaho 5.1

--- ErpBI/ETL/tools/src/build-mdxudf.bat
+++ ErpBI/ETL/tools/src/build-mdxudf.bat
@@ -1,2 +1,2 @@
-javac -cp ..\buildlib\commons-logging-1.0.4.jar;..\buildlib\mondrian.jar com\erpi\mdxudf\GetDateInfo.java
+javac -cp ..\buildlib\commons-logging-1.1.1.jar;..\buildlib\mondrian-3.7.0.0-752.jar com\erpi\mdxudf\GetDateInfo.java
 jar -cf ..\lib\mdxudf.jar com\erpi\mdxudf\GetDateInfo.class
